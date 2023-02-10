9c7162fad748dee103419502e0f28ceb769c7670
avengersscientist <abhiramitapr@gmail.com>
2023-02-10 10:44:46 -0500

    Commit

--- src/main/java/com/example/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/example/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -1,15 +1,28 @@
 package com.example.springcloudconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class SpringCloudConfigServerApplication {
+
+    @Value("${JAVA_HOME}")
+    private String java_home;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigServerApplication.class, args);
     }
+
+    @GetMapping("/java")
+    public String getHome(){
+        return java_home;
+    }
+
 
 }
